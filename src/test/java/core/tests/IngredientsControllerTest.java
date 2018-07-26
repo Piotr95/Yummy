a3c835299985b245b627cc152413f309064e085a
Piotr95 <piotrszczepniak#gmail.com>
2018-07-26 19:56:53 +0200

    better test

--- src/test/java/core/tests/IngredientsControllerTest.java
+++ src/test/java/core/tests/IngredientsControllerTest.java
@@ -1,17 +1,22 @@
 package core.tests;
 
+import core.services.IngredientService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -21,24 +26,19 @@ public class IngredientsControllerTest {
     private WebApplicationContext context;
 
     private MockMvc mockMvc;
+    @MockBean
+    IngredientService ingredientService;
 
     @Before
     public void init() {
     mockMvc= MockMvcBuilders.webAppContextSetup(context).build();
     }
     @Test
-    public void allIngredientsCanBeRetrived() throws Exception {
+    public void manyIngredientsCanBeRetrived() throws Exception {
         mockMvc.perform(get("/ingredient/all"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[\n" +
-                        "  {\n" +
-                        "    \"id\": 1,\n" +
-                        "    \"name\": \"cucumber\"\n" +
-                        "  },\n" +
-                        "  {\n" +
-                        "    \"id\": 2,\n" +
-                        "    \"name\": \"Garlic\"\n" +
-                        "  }\n" +
-                        "]"));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$").isArray());
+
     }
 }
